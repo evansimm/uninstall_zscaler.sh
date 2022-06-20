f4f5d4071246aea5247e823528bea45f304542cf
Evan Simmons <47922276+evansimm@users.noreply.github.com>
2022-06-20 10:15:38 -0400

    Update uninstall_zscaler.sh

--- uninstall_zscaler.sh
+++ uninstall_zscaler.sh
@@ -2,8 +2,6 @@
 #Created by Evan G. Simmons 06/15/22
 #Source: https://help.zscaler.com/client-connector/uninstalling-zscaler-client-connector
 
-#varible for Zscaler password loaded in to Jamf Pro.
-zscalerPW=$4
-
-/Applications/Zscaler/.Uninstaller.sh zscalerPW
+#varible for Zscaler password loaded in to Jamf Pro is $4.
+/Applications/Zscaler/.Uninstaller.sh $4
 exit
