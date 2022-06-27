e689e7d5d53fbc5b5cf1b629fb9d5fe8d3333046
Evan Simmons <47922276+evansimm@users.noreply.github.com>
2022-06-26 21:29:34 -0400

    Update uninstall_zscaler.sh

--- uninstall_zscaler.sh
+++ uninstall_zscaler.sh
@@ -2,6 +2,6 @@
 #Created by Evan G. Simmons 06/15/22
 #Source: https://help.zscaler.com/client-connector/uninstalling-zscaler-client-connector
 
-#varible for Zscaler password loaded in to Jamf Pro is $4.
+#Zscaler logout password must be loaded in to the policy distributing this script. It must be set as Parameter 4.
 /Applications/Zscaler/.Uninstaller.sh $4
 exit
